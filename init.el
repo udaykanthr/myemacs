c6f6499720f26bf9bd3911da47b452828786855f
Udaykanth Rapeti <udaykanth.rapeti@siliconimage.com>
2015-03-18 13:20:18 +0530

    added auto complete support.

--- init.el
+++ init.el
@@ -81,6 +81,9 @@
 (require 'gemacs-markdown)
 (require 'gemacs-git)
 (require 'gemacs-workspace)
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
 ;;(require 'gemacs-editing)
 
 (require 'yasnippet)
