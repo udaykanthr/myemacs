702bfa14d20288d8a8fc717c5dea1a9ccd58ea4c
Udaykanth R <udaykanth.rapeti@gmail.com>
2016-11-19 13:30:53 +0530

    old changess.

--- init.el
+++ init.el
@@ -238,6 +238,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(magit-diff-options nil)
  '(send-mail-function (quote smtpmail-send-it))
  '(smtpmail-smtp-server "smtp.gmail.com")
  '(smtpmail-smtp-service 587))
